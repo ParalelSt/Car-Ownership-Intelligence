2c1b65bab9c08a37460a6986ba5a2c0d9e091765
ParalelSt <aronddtt@gmail.com>
2026-03-02 19:04:18 +0100

    fix: input supports icons now, removed extra border

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -3,20 +3,26 @@ import { type ComponentPropsWithoutRef, forwardRef } from "react";
 interface InputProps extends ComponentPropsWithoutRef<"input"> {
   label: string;
   error?: string;
+  icon?: string;
 }
 
 const Input = forwardRef<HTMLInputElement, InputProps>(
-  ({ label, placeholder, className, error, ...props }, ref) => {
+  ({ label, placeholder, className, error, icon, ...props }, ref) => {
     return (
       <div className={`w-full ${className}`}>
         <label className="text-accent">{label}</label>
-        <input
-          className={`mt-2 mb-2 w-full h-10 p-2 border ${error ? "border-error" : "border-border"} rounded-base text-sm placeholder:text-text-muted`}
-          ref={ref}
-          placeholder={placeholder}
-          {...props}
-          autoComplete="off"
-        />
+        <div
+          className={`mt-2 mb-2 w-full h-10 border ${error ? "border-error" : "border-border"} rounded-base text-sm placeholder:text-text-muted`}
+        >
+          {icon ? icon : ""}
+          <input
+            className="p-2 w-full h-full text-sm placeholder:text-text-muted"
+            ref={ref}
+            placeholder={placeholder}
+            {...props}
+            autoComplete="off"
+          />
+        </div>
         {error ? (
           <span className="block mb-1 text-sm text-error">{error}</span>
         ) : (
